649596eab71c7f5f0e396f195eef1cdee935aa6b
Joshua Palacios <palacios.joshua@gmail.com>
2017-12-04 04:08:22 +0000

    automatically backed up by learn

--- app/models/ship.rb
+++ app/models/ship.rb
@@ -1,2 +1,11 @@
 class Ship
+  attr_accessor :name, :type, :class
+  @@ships = []
+
+  def initialize (params)
+    @name = params[:name]
+    @type = params[:type]
+    @class = params[:class]
+    @@ships << self
+  end
 end
