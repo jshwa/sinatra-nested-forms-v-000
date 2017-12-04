be6245ff35abaeecd937d848906aadef13aab16d
Joshua Palacios <palacios.joshua@gmail.com>
2017-12-04 04:11:08 +0000

    automatically backed up by learn

--- app/models/pirate.rb
+++ app/models/pirate.rb
@@ -1,11 +1,9 @@
 class Pirate
   attr_accessor :name, :weight, :height
-  @@all = []
 
   def initialize(params)
     @name = params[:name]
     @weight = params[:weight]
     @height = params[:height]
-    @@all << self
   end
 end
